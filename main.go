1ec0314c31c777f87ac37741ac7fba0f167d8d13
Jihan <jihanko42@gmail.com>
2022-02-09 23:49:48 -0500

    add cat api endpoint

--- main.go
+++ main.go
@@ -2,19 +2,31 @@ package main
 
 import (
 	"fmt"
-	"feline-bot/bot"
-	"feline-bot/config"
+	"encoding/json"
+	"flag"
+	"io/ioutil"
+	"net/http"
+	"os"
+	"os/signal"
+	"strings"
+	"syscall"
+	"github.com/bwmarrin/discordgo" 
 )
 
-func main() {
-	err := config.ReadConfig()
+var Token string
 
+const CAT_API_ENDPOINT = "https://api.thecatapi.com"
+
+func init() {
+	flag.StringVar(&Token, "t", "", "Bot Token")
+	flag.Parse()
+}
+
+func main() {
+	discord, err := discordgo.New("Bot " + Token)
 	if err != nil {
-		fmt.Println("CONFIG ERROR", err.Error())
+		fmt.Println("ERROR ESTABLISHING DISCORD SESSION: ", err)
 		return
 	}
-
-	bot.Start()
-
-	return
+	
 }
